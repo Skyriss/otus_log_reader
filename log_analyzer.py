258ff4b6ea78967d5ae11a3d0612dcc032e22d3e
Aleksandr Glazkov <farstone@yandex-team.ru>
2021-07-23 12:26:41 +0300

    Fixes after review

--- log_analyzer.py
+++ log_analyzer.py
@@ -6,6 +6,7 @@ import os
 import re
 import gzip
 import logging
+import statistics
 import sys
 from collections import defaultdict, namedtuple
 from string import Template
@@ -28,167 +29,135 @@ config = {
 }
 
 
-class File:
-    """Implements methods for working with text files"""
-
-    @staticmethod
-    def read_file(filename):
-        """Reads whole text file"""
-        with open(filename, "rt", encoding="utf-8") as open_file:
-            return open_file.read()
-
-    @staticmethod
-    def write_file_content(directory, filename, content):
-        """Writes content to a file"""
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
-        with open(os.path.join(directory, filename), "w", encoding="utf-8") as open_file:
-            open_file.write(content)
-
-    @staticmethod
-    def read_file_lines(filename):
-        """Reads logfile by lines"""
-        try:
-            file_open = gzip.open if filename.endswith(".gz") else open
-            with file_open(filename, "rt", encoding="utf-8") as open_file:
-                for line in open_file:
-                    yield line.strip()
-        except IOError as err:
-            logging.exception("During reading '%s' file error occurred: %s", filename, err)
-            raise
+def read_file(filename):
+    """Reads whole text file"""
+    with open(filename, "rt", encoding="utf-8") as open_file:
+        return open_file.read()
 
 
-class Log:
-    """Implements methods for logs reading"""
-    def __init__(self, log_dir):
-        self.log_dir = log_dir
+def write_file_content(filename, content):
+    """Writes content to a file"""
+    directory = os.path.dirname(filename)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
+    with open(filename, "w", encoding="utf-8") as open_file:
+        open_file.write(content)
+
+
+def read_file_lines(filename):
+    """Reads logfile by lines"""
+    try:
+        file_open = gzip.open if filename.endswith(".gz") else open
+        with file_open(filename, "rt", encoding="utf-8") as open_file:
+            for line in open_file:
+                yield line.strip()
+    except IOError as err:
+        logging.exception("During reading '%s' file error occurred: %s", filename, err)
+        raise
+
+
+def get_last_logfile(log_dir):
+    print(os.path.exists(log_dir))
+    if not os.path.exists(log_dir):
+        return None
     logfile_name_template = re.compile(r"^nginx-access-ui\.log-(?P<date>\d{8})(?P<extension>|\.gz)$")
     Logfile = namedtuple("Logfile", "filename date")
-
-    def _list_log_files(self, log_dir):
-        """Lists log files in directory"""
-        for file in os.listdir(log_dir):
-            matched = self.logfile_name_template.match(file)
-            if matched:
-                yield {
-                    "filename": file,
-                    "date": datetime.datetime.strptime(matched.groupdict()["date"], "%Y%m%d"),
-                }
-
-    def get_last_logfile(self):
-        """Finds out logifile with a highest datestamp in the name"""
-        if not os.path.exists(self.log_dir):
-            return None
-        last_logfile = self.Logfile("", "")
-        last_log_date = ""
-        for file in self._list_log_files(self.log_dir):
-            if not last_log_date:
-                last_log_date = file["date"]
-            if file["date"] > last_log_date:
-                last_logfile = self.Logfile(os.path.join(self.log_dir, file["filename"]), file["date"])
-                last_log_date = file["date"]
-        return last_logfile
-
-
-class Report:
-    """Implements methods for report generating"""
-    def __init__(self, report_dir, report_size, template_file):
-        self.template_file = template_file
-        self.directory = report_dir
-        self.size = report_size
-        self.name = ""
-        self.data = []
-
-    @staticmethod
-    def get_report_name(log_date):
-        """Generates report name"""
-        return "report-{}.html".format(log_date.strftime("%Y.%m.%d"))
-
-    def generate_report(self):
-        """Generates report from a template"""
-        try:
-            report_template = Template(File.read_file(self.template_file))
-        except IOError as err:
-            logging.exception("Can't read template file %s: %s", self.template_file, err)
-            raise
-        return report_template.safe_substitute(table_json=str(self.data))
-
-    @staticmethod
-    def _time_sort_func(time):
-        """Returns summarized request_time"""
-        return sum(time[1])
-
-    def generate_report_data(self, request_time_dict, total_request_count, total_request_time):
-        """Collects all data needed for report and put it into dict"""
-        request_stats = []
-        for request, time in sorted(request_time_dict.items(), key=self._time_sort_func, reverse=True):
-            request_stats.append({
-                "url": request,
-                "count": len(time),
-                "time_sum": sum(time),
-                "time_avg": sum(time)/len(time),
-                "time_max": max(time),
-                "time_med": median(time),
-                "time_perc": sum(time)/total_request_time,
-                "count_perc": len(time)/total_request_count})
-            if len(request_stats) == self.size:
-                break
-        return request_stats
-
-
-class LogParser:
-    """Implements method for parsing log files"""
-    def __init__(self, error_limit):
-        self.error_limit = error_limit
-
-    @staticmethod
-    def _parse_log_string(logline):
-        """Parses string of a log"""
-        splitted_string = logline.split(" ")
-        if not splitted_string[7].startswith("/") and not splitted_string[7].startswith("http"):
-            raise ValueError("URL not found")
-        return splitted_string[7], float(splitted_string[-1])
-
-    def parse_logfile(self, log_filename):
-        """Parses log file and calculates total values"""
-        request_time_dict = defaultdict(list)
-        total_request_count = 0
-        total_request_time = 0
-        err_count = 0
-        for line in File.read_file_lines(log_filename):
+    last_logfile = Logfile("", "")
+    last_log_date = ""
+    print(os.listdir(log_dir))
+    for file in os.listdir(log_dir):
+        matched = logfile_name_template.match(file)
+        if matched:
             try:
-                url, load_time = self._parse_log_string(line)
-            except (IndexError, ValueError) as err:
-                logging.error("Got error while parsing '%s': %s", line, err)
-                err_count += 1
+                file_date = datetime.datetime.strptime(matched.groupdict()["date"], "%Y%m%d")
+            except ValueError:
+                logging.exception(
+                    "Cannot convert '%s' to datetime. Skipping '%s' file",
+                    matched.groupdict()["date"],
+                    file
+                )
                 continue
-            if url not in request_time_dict:
-                request_time_dict[url] = []
-            request_time_dict[url].append(load_time)
-            total_request_count += 1
-            total_request_time += load_time
-
-        error_rate = 100 * err_count / total_request_count
-        if error_rate > self.error_limit:
-            raise ValueError("Parsing error budget exceeded! Failed to parse more than {}% ({}/{}) requests".format(
-                error_rate,
-                err_count,
-                total_request_count
-            ))
-        logging.debug("Found %s parsing errors in %s lines", err_count, total_request_count)
-        return request_time_dict, total_request_count, total_request_time
+            if not last_log_date or file_date >= last_log_date:
+                last_log_date = file_date
+                last_logfile = Logfile(os.path.join(log_dir, file), file_date)
+    return last_logfile
 
 
-def median(lst):
-    """Calculates median of a list"""
-    sorted_lst = sorted(lst)
-    lst_len = len(lst)
-    index = (lst_len - 1) // 2
-    if lst_len % 2:
-        return sorted_lst[index]
-    return (sorted_lst[index] + sorted_lst[index + 1])/2.0
+def get_report_name(log_date):
+    """Generates report name"""
+    return "report-{}.html".format(log_date.strftime("%Y.%m.%d"))
+
+
+def generate_report_data(request_time_dict, total_request_count, total_request_time, report_size):
+    """Collects all data needed for report and put it into dict"""
+
+    def _time_sort_func(time_list):
+        """Returns summarized request_time"""
+        return sum(time_list[1])
+
+    request_stats = []
+    for request, time in sorted(request_time_dict.items(), key=_time_sort_func, reverse=True):
+        request_stats.append({
+            "url": request,
+            "count": len(time),
+            "time_sum": sum(time),
+            "time_avg": sum(time)/len(time),
+            "time_max": max(time),
+            "time_med": statistics.median(time),
+            "time_perc": sum(time)/total_request_time,
+            "count_perc": len(time)/total_request_count})
+        if len(request_stats) == report_size:
+            break
+    return request_stats
+
+
+def generate_report(template_file, report_data):
+    """Generates report from a template"""
+    try:
+        report_template = Template(read_file(template_file))
+    except IOError as err:
+        logging.exception("Can't read template file %s: %s", template_file, err)
+        raise
+    return report_template.safe_substitute(table_json=str(report_data))
+
+
+def parse_log_string(logline):
+    """Parses string of a log"""
+    splitted_string = logline.split(" ")
+    if not splitted_string[7].startswith("/") and not splitted_string[7].startswith("http"):
+        raise ValueError("URL not found")
+    return splitted_string[7], float(splitted_string[-1])
+
+
+def parse_logfile(log_filename, error_limit):
+    """Parses log file and calculates total values"""
+    request_time_dict = defaultdict(list)
+    total_request_count = 0
+    total_request_time = 0
+    err_count = 0
+    for line in read_file_lines(log_filename):
+        try:
+            url, load_time = parse_log_string(line)
+        except (IndexError, ValueError) as err:
+            logging.error("Got error while parsing '%s': %s", line, err)
+            err_count += 1
+            continue
+        if url not in request_time_dict:
+            request_time_dict[url] = []
+        request_time_dict[url].append(load_time)
+        total_request_count += 1
+        total_request_time += load_time
+
+    error_rate = 100 * err_count / total_request_count
+    if error_rate > error_limit:
+        raise RuntimeError("Parsing error limit exceeded! Failed to parse more than {}% ({}/{}) requests".format(
+            error_rate,
+            err_count,
+            total_request_count
+        ))
+    logging.debug("Found %s parsing errors in %s lines", err_count, total_request_count)
+    return request_time_dict, total_request_count, total_request_time
 
 
 def parse_args():
@@ -198,23 +167,19 @@ def parse_args():
     return vars(parser.parse_args())
 
 
-def parse_config_file(config_file):
-    """Reads config file and update script config"""
-    file_content = File.read_file(config_file)
-    if file_content:
-        new_config = yaml.load(file_content, Loader=yaml.SafeLoader)
-        for key, value in new_config.items():
-            yield {key: value}
-
-
-def update_config(config_file):
-    """Updates values in config variable with values from file"""
+def gen_config(config_file):
+    """Updates values in config variable with values from config file"""
+    local_config = config
     try:
-        for item in parse_config_file(config_file):
-            config.update(item)
+        file_content = read_file(config_file)
+        if file_content:
+            new_config = yaml.load(file_content, Loader=yaml.SafeLoader)
+            for key, value in new_config.items():
+                local_config.update({key: value})
     except (IOError, TypeError) as err:
         logging.exception("Error while reading config file: %s", err)
-        sys.exit(1)
+        raise
+    return local_config
 
 
 def set_logging(log_level, log_file=None):
@@ -230,54 +195,53 @@ def set_logging(log_level, log_file=None):
 def main():
     valid_logging_levels = ["info", "error", "exception"]
     args = parse_args()
-    if "config" in args:
-        update_config(args["config"])
-    if config["LOGGING_LEVEL"] not in valid_logging_levels:
+    try:
+        configuration = gen_config(args["config"])
+    except (IOError, TypeError):
+        sys.exit(1)
+    if configuration["LOGGING_LEVEL"] not in valid_logging_levels:
         raise ValueError("'{}' is not valid logging level. Choose one of {}".format(
-            config["LOGGING_LEVEL"],
+            configuration["LOGGING_LEVEL"],
             valid_logging_levels)
         )
-    set_logging(config["LOGGING_LEVEL"], config["LOG_FILE"])
+    set_logging(configuration["LOGGING_LEVEL"], configuration["LOG_FILE"])
 
-    logfile = Log(config["LOG_DIR"])
-    last_logfile = logfile.get_last_logfile()
+    last_logfile = get_last_logfile(configuration["LOG_DIR"])
     if not last_logfile.filename:
-        logging.info("No logfiles found in %s", config["LOG_DIR"])
+        logging.info("No logfiles found in %s", configuration["LOG_DIR"])
         sys.exit()
     logging.info("The latest logfile: %s", last_logfile.filename)
 
-    parser = LogParser(config["PARSING_ERROR_LIMIT"])
-
-    report = Report(
-        config["REPORT_DIR"],
-        config["REPORT_SIZE"],
-        config["TEMPLATE_FILENAME"]
-    )
-    report.name = report.get_report_name(last_logfile.date)
-    if os.path.exists(report.directory) and report.name in os.listdir(report.directory):
-        logging.info("Nothing to do. Report %s already exists", os.path.join(report.directory, report.name))
+    report_file = os.path.join(configuration["REPORT_DIR"], get_report_name(last_logfile.date))
+    if os.path.exists(report_file):
+        logging.info("Nothing to do. Report '%s' already exists", report_file)
         sys.exit()
+
     try:
-        request_time_dict, total_request_count, total_request_time = parser.parse_logfile(str(last_logfile.filename))
-    except KeyError as err:
+        request_time_dict, total_request_count, total_request_time = parse_logfile(
+            str(last_logfile.filename),
+            configuration["PARSING_ERROR_LIMIT"]
+        )
+    except RuntimeError as err:
         logging.exception("During parsing request data an error occurred: %s", err)
         sys.exit(1)
     except IOError:
         sys.exit(1)
 
-    report.data = report.generate_report_data(
+    report_data = generate_report_data(
         request_time_dict,
         total_request_count,
-        total_request_time
+        total_request_time,
+        configuration["REPORT_SIZE"]
     )
 
     try:
-        generated_report = report.generate_report()
+        generated_report = generate_report(configuration["TEMPLATE_FILENAME"], report_data)
     except IOError:
         sys.exit(1)
 
-    File.write_file_content(report.directory, report.name, generated_report)
-    logging.info("Done! Report saved to %s", os.path.join(config["REPORT_DIR"], report.name))
+    write_file_content(report_file, generated_report)
+    logging.info("Done! Report saved to %s", report_file)
 
 
 if __name__ == "__main__":
--- test_log_analyzer.py
+++ test_log_analyzer.py
@@ -3,22 +3,22 @@ import datetime
 from collections import namedtuple
 from unittest import mock, TestCase
 from log_analyzer import (
-    File,
-    Log,
-    LogParser,
-    Report,
-    median,
+    read_file_lines,
+    get_last_logfile,
+    get_report_name,
+    generate_report,
+    generate_report_data,
+    parse_log_string,
+    parse_logfile,
 )
 
 
 def mock_empty_parse_args():
     return vars()
 
+
 class TestLogAnalyzer(TestCase):
-    logfile = Log("")
-    log_parser = LogParser(50)
-    report = Report(report_dir="", report_size=5, template_file="")
-    report.data = [
+    report_data = [
         {'url': '/api/',
          'count': 2,
          'time_sum': 17,
@@ -41,7 +41,7 @@ class TestLogAnalyzer(TestCase):
                "nginx-access-ui.log-20210630.gz",
                ]
     Logfile = namedtuple("Logfile", "filename date")
-    valid_last_log = Logfile("nginx-access-ui.log-20210630", datetime.datetime(2021, 6, 30, 0, 0))
+    valid_last_log = Logfile("nginx-access-ui.log-20210630.gz", datetime.datetime(2021, 6, 30, 0, 0))
     log_sample = """1.170.209.160 -  - [29/Jun/2017:03:50:33 +0300] "GET /export/appinstall_raw/2017-06-30/ HTTP/1.0" 404 162 "-" "Mozilla/5.0 (Windows; U; Windows NT 6.0; ru; rv:1.9.0.12) Gecko/2009070611 Firefox/3.0.12 (.NET CLR 3.5.30729)" "-" "-" "-" 0.001
 1.165.177.32 -  - [29/Jun/2017:03:50:33 +0300] "GET /export/appinstall_raw/2017-06-29/ HTTP/1.0" 200 28358 "-" "Mozilla/5.0 (Windows; U; Windows NT 6.0; ru; rv:1.9.0.12) Gecko/2009070611 Firefox/3.0.12 (.NET CLR 3.5.30729)" "-" "-" "-" 0.003
 1.165.177.32 -  - [29/Jun/2017:03:50:33 +0300] "GET /export/appinstall_raw/2017-06-30/ HTTP/1.0" 404 162 "-" "Mozilla/5.0 (Windows; U; Windows NT 6.0; ru; rv:1.9.0.12) Gecko/2009070611 Firefox/3.0.12 (.NET CLR 3.5.30729)" "-" "-" "-" 0.001"""
@@ -51,6 +51,7 @@ class TestLogAnalyzer(TestCase):
     }
     valid_total_request_count = 3
     valid_total_request_time = 0.005
+    valid_report_size = 2
     valid_report_data = [
         {
             "url": "/export/appinstall_raw/2017-06-29/",
@@ -74,45 +75,17 @@ class TestLogAnalyzer(TestCase):
         }
     ]
 
-    def test_median(self):
-        lst = [1, 3, 4, 4, 12]
-        mediana = median(lst)
-        self.assertEqual(mediana, 4)
-
-    def test_list_log_files(self):
-        log_files = [
-            {"filename": "nginx-access-ui.log-20170630",
-             "date": datetime.datetime(2017, 6, 30, 0, 0),
-             },
-            {"filename": "nginx-access-ui.log-20170630.gz",
-             "date": datetime.datetime(2017, 6, 30, 0, 0),
-             },
-            {"filename": "nginx-access-ui.log-20210630",
-             "date": datetime.datetime(2021, 6, 30, 0, 0),
-            },
-            {"filename": "nginx-access-ui.log-20210630.gz",
-             "date": datetime.datetime(2021, 6, 30, 0, 0),
-             }
-                     ]
-        with mock.patch("os.listdir") as mocked_listdir:
-            mocked_listdir.return_value = self.listdir
-            counter = 0
-            for item in self.logfile._list_log_files("some_dir"):
-                counter += 1
-                self.assertIn(item, log_files)
-            self.assertEqual(len(log_files), counter)
-
     def test_get_last_logfile(self):
-        self.assertEqual(self.logfile.get_last_logfile(), None)
         with mock.patch("os.listdir") as mocked_listdir:
             mocked_listdir.return_value = self.listdir
             with mock.patch("os.path.exists") as mocked_path_exists:
                 mocked_path_exists.return_value = True
-                self.assertEqual(self.logfile.get_last_logfile(), self.valid_last_log)
+                last_logfile = get_last_logfile("")
+                self.assertEqual(self.valid_last_log, last_logfile)
 
     def test_get_logfile_lines(self):
-        with mock.patch("builtins.open", mock.mock_open(read_data="data")) as mock_file:
-            for line in File.read_file_lines(str(self.valid_last_log)):
+        with mock.patch("builtins.open", mock.mock_open(read_data="data")):
+            for line in read_file_lines(str(self.valid_last_log)):
                 self.assertEqual(line, "data")
 
     def test_parse_log_string(self):
@@ -121,37 +94,35 @@ class TestLogAnalyzer(TestCase):
         invalid_log_string = "not_a_log"
         invalid_log_string2 = "1.196.116.32 -  - [29/Jun/2017:03:50:29 +0300] \"GET /api/v2/banner/782125 HTTP/1.1\" 200 1052 \"-\" \"Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5\" \"-\" \"1498697426-2190034393-4708-9752865\" \"dc7161be3\""
         invalid_log_string3 = "1.196.116.32 -  - [29/Jun/2017:03:50:29 +0300] \"-\" 200 1052 \"-\" \"Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5\" \"-\" \"1498697426-2190034393-4708-9752865\" \"dc7161be3\" 2.450"
-        self.assertEqual(self.log_parser._parse_log_string(valid_log_string), parsed_log_string)
+        self.assertEqual(parse_log_string(valid_log_string), parsed_log_string)
         with self.assertRaises(IndexError):
-            self.log_parser._parse_log_string(invalid_log_string)
+            parse_log_string(invalid_log_string)
         with self.assertRaises(ValueError):
-            self.log_parser._parse_log_string(invalid_log_string2)
+            parse_log_string(invalid_log_string2)
         with self.assertRaises(ValueError):
-            self.log_parser._parse_log_string(invalid_log_string3)
+            parse_log_string(invalid_log_string3)
 
     def test_get_report_name(self):
-        self.assertEqual(self.report.get_report_name(self.valid_last_log.date), "report-2021.06.30.html")
+        self.assertEqual(get_report_name(self.valid_last_log.date), "report-2021.06.30.html")
 
     def test_generate_report(self):
         valid_report = "This report contains data: [{'url': '/api/', 'count': 2, 'time_sum': 17, 'time_avg': 3, 'time_max': 4, 'time_med': 4, 'time_perc': 0.0, 'count_perc': 0.0}] as list"
         with mock.patch("builtins.open", mock.mock_open(read_data="This report contains data: $table_json as list")) as mock_file:
-            self.assertEqual(self.report.generate_report(), valid_report)
+            """template_file, report_data"""
+            self.assertEqual(generate_report(mock_file, self.report_data), valid_report)
 
     def test_generate_report_data(self):
-        generated_report_data = self.report.generate_report_data(
+        generated_report_data = generate_report_data(
             self.valid_request_time_dict,
             self.valid_total_request_count,
-            self.valid_total_request_time
+            self.valid_total_request_time,
+            self.valid_report_size,
         )
         self.assertEqual(generated_report_data, self.valid_report_data)
 
     def test_parse_logfile(self):
         with mock.patch("builtins.open", mock.mock_open(read_data=self.log_sample)):
-            request_time_dict, total_request_count, total_request_time = self.log_parser.parse_logfile("some_file")
-            print(request_time_dict)
+            request_time_dict, total_request_count, total_request_time = parse_logfile("some_file", error_limit=50)
             self.assertEqual(request_time_dict, self.valid_request_time_dict)
             self.assertEqual(total_request_count, self.valid_total_request_count)
             self.assertEqual(total_request_time, self.valid_total_request_time)
-
-
-
